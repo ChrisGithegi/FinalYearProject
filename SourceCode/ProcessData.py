b52c41b71622adfac1f2394a299b436176530a1f
Christopher <christophergithegi@outlook.com>
2020-10-24 13:40:26 +0100

    processed data

--- SourceCode/ProcessData.py
+++ SourceCode/ProcessData.py
@@ -1,28 +1,37 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import os, sys
 import csv
 
-os.chdir("/home/users/sc014212/Documents/FinalYearProject/RawData")
+os.chdir("../RawData")
 dirs = os.listdir()
 #recursively go through raw data and combine into one .csv file
+writer = csv.writer(open('../ProcessedData/ProcessedData.csv','wt'))
 for d in dirs:
+	x = 0
 	try:
-		e = "{}/{}".format(os.getcwd(),d) 
+		e = "{}/{}".format(os.getcwd(),d)
 		os.chdir(e)
+		print(os.getcwd())
 		try:
 			c = os.listdir()
 			c2 = []
-			with open(c) as csvfile:
+			with open(c[0],'rt',encoding='ISO-8859-1') as csvfile:
 				anprData = csv.reader(csvfile)
-				print(c)
-				for row in anprData:
-					c2.append(row)
-				print(len(c2))
-		except:
-			pass
-		os.chdir("/home/users/sc014212/Documents/FinalYearProject/RawData")
-		
+				try:
+					if x == 0:
+						for row in anprData:
+							writer.writerow(row)
+					else:
+						next(anprData)
+						for row in anprData:
+							writer.writerow(row)
+				except csv.Error as e:
+					print(e)
+		except Exception as e:
+			print(e)
+		os.chdir("../")
+
 	except:
 		pass
-
+	x+=1
